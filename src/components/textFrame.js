027ec0b2c6adcdd2272f4e708c04a29bf6cae682
Suzanne Cabral <suzannecabral@live.com>
2020-09-11 09:02:10 -0700

    Styled components is working, added reset to index.css

--- src/components/textFrame.js
+++ src/components/textFrame.js
@@ -1,17 +1,60 @@
 import React from "react";
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 
-// const styledTextFrame = styled.div`
+const StyledTextFrame = styled.div`
+  background-color:#101012;
+  color:#fafafa;
+  font-family:"Ubuntu", sans-serif;
 
-// `
+
+  h1 {
+  font-size:2.8rem;
+  margin: 2rem 0 0 0 ;
+  order:10;
+  
+}
+h2 {
+  font-size:2.3rem;
+  font-weight:400;
+  margin: 0 0 1.2rem 0;
+}
+h3 {
+  margin:0 0 .5rem 0;
+}
+h3, .author {
+  font-size:1.2rem;
+  color:#66667f;
+  font-weight:bold;
+}
+p {
+  font-size:1.4rem;
+  font-weight:400;
+  line-height:1.7rem;
+  letter-spacing:.1px;
+  margin:0 0 1rem 0;
+  
+}
+
+a {
+    text-decoration:none;
+    font-size:1.4rem;
+    font-weight:bold;
+    color:white;
+}
+
+.author {
+  border-bottom: 1px solid #66667f;
+  padding-bottom: 2rem;
+}
+`
 const TextFrame = props => {
 
     const { imgTitle, imgDate, imgAuthor, imgDesc } = props
 
 
     return (
-
+        <StyledTextFrame>
         <div className="textFrame">
           <h1>NASA Image of the Day</h1>
           <h2>{imgTitle}</h2>
@@ -20,6 +63,7 @@ const TextFrame = props => {
           <p>{imgAuthor}</p>
           <p>{imgDesc}</p>
         </div>
+        </StyledTextFrame>
 
     )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,4 @@
+//theme object here
+export default {
+    //css rules in JS syntax go here
+}
